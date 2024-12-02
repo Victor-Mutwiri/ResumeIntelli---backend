f59150ee047386c99e7cac247c1309af080345e0
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-12-02 17:54:35 +0300

    modified temp uploads

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 from config import Config
+import os
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     CORS(app)  # Enable CORS for all routes
 
+
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+    
     from app.routes import api
     app.register_blueprint(api)
 
--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 class Config:
     GROQ_API_KEY = os.getenv('GROQ_API_KEY')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file size
-    UPLOAD_FOLDER = 'temp_uploads'
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'temp_uploads')
